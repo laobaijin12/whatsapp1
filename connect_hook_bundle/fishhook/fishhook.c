e95cafb26dd5178abcfcc15373d23989acad6010
laobaijin12 <laobaijin1@outlook.com>
2025-06-23 19:29:43 +0700

    Delete connect_hook_bundle directory

--- connect_hook_bundle/fishhook/fishhook.c
+++
@@ -1,9 +0,0 @@
-
-#include "fishhook.h"
-#include <stdio.h>
-
-int rebind_symbols(struct rebinding rebindings[], size_t rebindings_nel) {
-  // 仅模拟函数定义，用于绕过链接错误
-  printf("rebind_symbols called (mock)\n");
-  return 0;
-}
